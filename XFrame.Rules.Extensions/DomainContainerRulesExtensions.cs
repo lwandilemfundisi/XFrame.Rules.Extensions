5cab89503682d5b7e8255766fb992e13e5b3fd1c
Lwandile Mfundisi <Lwandile.Mfundisi@media24.com>
2023-09-11 09:59:11 +0200

    Add project files.

---
+++ XFrame.Rules.Extensions/DomainContainerRulesExtensions.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+using XFrame.DomainContainers;
+using XFrame.Rules.Validations;
+
+namespace XFrame.Rules.Extensions
+{
+    public static class DomainContainerRulesExtensions
+    {
+        public static IDomainContainer AddRules(
+            this IDomainContainer domainContainer,
+            Assembly fromAssembly,
+            Predicate<Type> predicate = null)
+        {
+            predicate = predicate ?? (t => true);
+            var ruleTypes = fromAssembly
+                .GetTypes()
+                .Where(t => t.IsRule())
+                .Where(t => predicate(t));
+
+            foreach (var ruleType in ruleTypes)
+            {
+                domainContainer
+                    .ServiceCollection
+                    .AddTransient(ruleType);
+            }
+
+            return domainContainer
+            .AddTypes(ruleTypes);
+        }
+
+        private static bool IsRule(this Type type)
+        {
+            return typeof(IValidation).IsAssignableFrom(type) && !type.IsAbstract && !type.IsInterface;
+        }
+    }
+}
